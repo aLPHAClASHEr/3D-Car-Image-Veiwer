2bc4a8e091ab43b1252d1892748c095564b785d7
aLPHAClASHEr <73337212+aLPHAClASHEr@users.noreply.github.com>
2021-07-30 11:45:26 +0530

    Add files via upload

---
+++ js/get.js
@@ -0,0 +1,30 @@
+
+        import { GLTFLoader } from './GLTFLoader.js';
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            .01,
+            1000
+        );
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        var loader = new GLTFLoader();
+        var obj;
+        loader.load('../img4/scene.gltf', function (gltf) {
+            obj = gltf.scene;
+            scene.add(gltf.scene);
+
+        });
+        scene.background = new THREE.Color(0x181f2b)
+        var light = new THREE.HemisphereLight(0xfffffff, 0x000000, 1);
+        scene.add(light);
+        camera.position.set(0, 0.5, 14);
+        function animate() {
+            requestAnimationFrame(animate);
+            obj.rotation.y += 0.03;
+            renderer.render(scene, camera);
+        }
+        animate();
---
+++ js/get2.js
@@ -0,0 +1,30 @@
+
+        import { GLTFLoader } from './GLTFLoader.js';
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            .01,
+            1000
+        );
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        var loader = new GLTFLoader();
+        var obj;
+        loader.load('../img6/scene.gltf', function (gltf) {
+            obj = gltf.scene;
+            scene.add(gltf.scene);
+
+        });
+        scene.background = new THREE.Color(0x181f2b)
+        var light = new THREE.HemisphereLight(0xffffff, 0x000000, 15);
+        scene.add(light);
+        camera.position.set(0, 0.5, 3.5);
+        function animate() {
+            requestAnimationFrame(animate);
+            obj.rotation.y += 0.03;
+            renderer.render(scene, camera);
+        }
+        animate();
